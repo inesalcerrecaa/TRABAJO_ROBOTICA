69a8c2de600b4bd98cff513d6eafaa93d78714b2
maria <m.heredero@alumnos.upm.es>
2026-04-19 13:00:18 +0200

    avanzo en kinematics.c

--- Trabajo_Robotica_STM32/Core/Src/kinematics.c
+++ Trabajo_Robotica_STM32/Core/Src/kinematics.c
@@ -1,8 +1,19 @@
 /*
- * kinematics.c
+ * kinematics.c -> cinematica inversa del robot SCARA 4-Dof
  *
  *  Created on: Mar 30, 2026
- *      Author: gfernandez
+ *      Author: maria
  */
+
+/* USER CODE BEGIN Includes */
+#include "kinematics.h"
+#include <math.h>      /* sqrtf, atan2f, acosf — usa versiones float */
+/* USER CODE END Includes */
+
+/* USER CODE BEGIN Private Variables */
+/* Variables internas del brazo efectivo (se calculan con IK_Update_Effective_Arm) */
+static float s_L_eff = L2 + L_XY;  /* Valor inicial con q4=0 */
+static float s_alpha  = 0.0f;
+/* USER CODE END Private Variables */
 
 
