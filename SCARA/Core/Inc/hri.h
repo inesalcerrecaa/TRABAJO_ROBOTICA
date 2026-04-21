a85a6e40f94e7c96d2fe08ec2e58ead6911abfba
Sofia18-10 <sofia.jara.quinones@alumnos.upm.es>
2026-04-21 22:09:52 +0200

    el hri compila perfecto

--- SCARA/Core/Inc/hri.h
+++ SCARA/Core/Inc/hri.h
@@ -8,6 +8,15 @@
 #ifndef INC_HRI_H_
 #define INC_HRI_H_
 
+#include "main.h"
 
+//funciones de pantalla
+void LCD_Init(void);
+void Display_LCD_Escribir(uint8_t fila, uint8_t col, char *texto);
+
+//funciones de botones y sensores
+int Leer_Botones_Accion(void);
+int Leer_Boton_Reset(void);
+int Leer_Sensor_Hall(void);
 
 #endif /* INC_HRI_H_ */
